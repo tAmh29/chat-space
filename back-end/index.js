c56bbdd656f52bc5a0db28a6bae44b8e6298b89f
tAmh <thuynh204@csu.fullerton.edu>
2024-05-19 16:35:45 -0700

    updated

--- back-end/index.js
+++ back-end/index.js
@@ -61,7 +61,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Chat Space API");
